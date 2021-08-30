a7f75c7f46cb3e3c9e2a10cb8fee0897c223bda2
phoenixxx1991 <phoenixxx1991@gmail.com>
2021-08-30 18:28:08 +0200

    Training #6.2

---
+++ src/labs/pm/data/Drink.java
@@ -0,0 +1,10 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+
+public class Drink extends Product{
+
+    public Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
+}

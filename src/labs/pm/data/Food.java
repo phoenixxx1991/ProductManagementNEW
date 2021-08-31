c05136c32e06dd8269c757f21fc3998599f9df80
phoenixxx1991 <phoenixxx1991@gmail.com>
2021-08-31 09:39:31 +0200

    Training #6

---
+++ src/labs/pm/data/Food.java
@@ -0,0 +1,33 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Food extends Product{
+
+    private LocalDate bestBefore;
+
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
+
+    Food(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
+        super(id, name, price, rating);
+        this.bestBefore = bestBefore;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+", "+bestBefore;
+    }
+
+    @Override
+    public BigDecimal getDiscount() {
+        return (bestBefore.equals(LocalDate.now())?super.getDiscount():BigDecimal.ZERO);
+    }
+
+    @Override
+    public Product applyRating(Rating newRating) {
+        return new Food(getId(),getName(),getPrice(),newRating,bestBefore);
+    }
+}
